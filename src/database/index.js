b978cd150e4dd447c627cfda34b8d7bbab09c6ef
Luiz henrique <94985541+LuizHenr1que@users.noreply.github.com>
2024-03-18 17:50:18 -0300

    🚀 :rocket:

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+
+const cliente = new Client({
+host: 'localhost',
+port: 5432,
+user: 'root',
+password: 'root',
+database: 'mycontacts',
+});
+
+cliente.connect();
+
+exports.query = async (query, values) => {
+  const { rows } = await cliente.query(query, values);
+  return rows;
+
+}
